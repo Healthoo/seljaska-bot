3b482878332305b32be3e587a0a49a84c71b18cb
hockiprinc <tkodric99@gmail.com>
2022-10-14 00:27:11 +0200

    main

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+# Import the os module.
+import os
+# Import load_dotenv function from dotenv module.
+from dotenv import load_dotenv
+# Loads the .env file that resides on the same level as the script.
+load_dotenv()
+# Grab the API token from the .env file.
+discord_token = os.getenv(key="key")
+
+intents = discord.Intents().all()
+intents.members = True
+intents.messages = True
+bot = discord.Client(intents= intents)
+
+
+@bot.event
+async def on_ready():
+   await bot.get_channel(606576973619134495).send("KI STE PIČKE")
+
+@bot.event
+async def on_message(message):
+    # CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+    if "vilec" in message.content.lower():
+    # SENDS BACK A MESSAGE TO THE CHANNEL.
+        await message.reply("L + ratio")
+
+# EXECUTES THE BOT WITH THE SPECIFIED TOKEN.
+bot.run(discord_token)
