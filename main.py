04e1946ac72c67ad74b3599da4e20623f0cee379
hockiprinc <tkodric99@gmail.com>
2022-10-14 20:52:45 +0200

    NE VEM

--- main.py
+++ main.py
@@ -8,26 +8,39 @@ copypasta = c.copypasta
 responses = c.responses
 
 # Client functionality, not moraš enablat intente
-bot = discord.Client(intents= discord.Intents().all())
-bot_2 =commands.Bot(intents=discord.Intents.all(), command_prefix="%")
+bot = discord.Client(intents=discord.Intents().all())
+bot_2 = commands.Bot(intents=discord.Intents.all(), command_prefix="%")
+
 
 # Event 1, bot pozdravi
 @bot.event
 async def on_ready():
-   await bot.get_channel(606576973619134495).send("FREŠER = SUPREME OVERLORD", file=discord.File('chungus.png'))
+    await bot.get_channel(606576973619134495).send("FREŠER = SUPREME OVERLORD", file=discord.File('chungus.png'))
+
 
 # Event 2, odgovori na keywords
 @bot.event
 async def on_message(message, ctx):
     content = message.content.lower()
+    for x in responses.keys():
+        index = content.find(x)
+        found = ""
+
+        if index != -1:
+            found = content[index:index + len(x)]
+        if found in responses and message.author.bot != True:
+            await message.reply(responses[found])
+
     if str(ctx.message.author == "bicmac the police man#4039"):
         await message.reply("Ti nisi moj šef")
     elif content in responses and message.author.bot != True:
         await message.reply(responses[content])
 
+
 @bot_2.command(name="unleashthekraken")
 async def lol(ctx):
     await ctx.channel.send(copypasta)
+
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN.
 bot.run(discord_token)
